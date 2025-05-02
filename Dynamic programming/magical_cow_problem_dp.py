4aac45a1e0e971a50f68323c31b6ff456707db1a
VArakelyan <vardan.g.arakelyan@gmail.com>
2025-05-02 11:48:22 +0400

    magical cows added

--- Dynamic programming/magical_cow_problem_dp.py
+++ Dynamic programming/magical_cow_problem_dp.py
@@ -6,9 +6,10 @@ n = 4
 
 m = 1
 
-queries = [1]
+queries = [5]
 
 initial_cows = [1,3,2,1]
+dp = [[0 for _ in range(c + 1)] for _ in range(max_days + 1)]
 
 def solve(c, n, m, queries, max_days, initial_cows):
 
@@ -18,7 +19,7 @@ def solve(c, n, m, queries, max_days, initial_cows):
             farms += dp[day][i]
         return farms
 
-    dp = [[0 for _ in range(c + 1)] for _ in range(max_days + 1)]
+
     for i in range(n):
         dp[0][initial_cows[i]] += 1
 
@@ -33,6 +34,6 @@ def solve(c, n, m, queries, max_days, initial_cows):
         day = queries[i]
         print(sumRows(dp, day))
 
-solve(c, n, m, queries, max_days, initial_cows)
 
+solve(c, n, m, queries, max_days, initial_cows)
 
